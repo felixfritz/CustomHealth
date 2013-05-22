6417885d0a62254ec658bcf91b10a0f794e5815f
felixfritz <geigenmusikant@yahoo.de>
2013-05-22 23:41:10 +0200

    Added static getPlugin method

--- src/at/felixfritz/customhealth/CustomHealth.java
+++ src/at/felixfritz/customhealth/CustomHealth.java
@@ -1,6 +1,7 @@
 package at.felixfritz.customhealth;
 
 import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 import at.felixfritz.customhealth.command.CustomCommand;
@@ -11,22 +12,25 @@ import at.felixfritz.customhealth.foodtypes.FoodDataBase;
 
 public class CustomHealth extends JavaPlugin {
 	Logger log = Logger.getLogger("Minecraft");
+	private static CustomHealth plugin;
 	
 	@Override
 	public void onEnable() {
 		
+		plugin = this;
+		
 		/*
 		 * Save all the configuration in plugins/CustomHealth/config.yml
 		 */
-		this.saveDefaultConfig();
-		this.getConfig().options().copyDefaults(true);
+		plugin.saveDefaultConfig();
+		plugin.getConfig().options().copyDefaults(true);
 		// Include the effects.txt file
-		this.saveResource("effects.txt", true);
+		plugin.saveResource("effects.txt", true);
 		
 		/*
 		 * CommandExecutor, currently only one command
 		 */
-		this.getCommand("chealth").setExecutor(new CustomCommand(this));
+		plugin.getCommand("chealth").setExecutor(new CustomCommand());
 		
 		/*
 		 * Make everything ready in the FoodDataBase. It just creates
@@ -37,23 +41,35 @@ public class CustomHealth extends JavaPlugin {
 		/*
 		 * Register the eating event, called as soon as someone eats something, e.g. a fish, carrot or hamburger
 		 */
-		this.getServer().getPluginManager().registerEvents(new EatingEvent(this.getConfig()), this);
+		plugin.getServer().getPluginManager().registerEvents(new EatingEvent(), plugin);
 		
 		/*
 		 * Check, if the two events about changing the food level and changing the health level
 		 * should be enabled or not.
 		 */
-		if(!this.getConfig().getBoolean("settings.change-food-level"))
-			this.getServer().getPluginManager().registerEvents(new FoodEvent(), this);
+		if(!plugin.getConfig().getBoolean("settings.change-food-level"))
+			plugin.getServer().getPluginManager().registerEvents(new FoodEvent(), plugin);
 		
-		if(!this.getConfig().getBoolean("settings.regain-health"))
-			this.getServer().getPluginManager().registerEvents(new HealthEvent(), this);
+		if(!plugin.getConfig().getBoolean("settings.regain-health"))
+			plugin.getServer().getPluginManager().registerEvents(new HealthEvent(), plugin);
 		
-		log.info("[" + this.getDescription().getName() + "] v" + this.getDescription().getVersion() + " is ready.");
+		
+		log.info("[" + plugin.getDescription().getName() + "] v" + plugin.getDescription().getVersion() + " is ready.");
 	}
 	
 	@Override
 	public void onDisable() {
+		plugin.saveDefaultConfig();
 		log.info("Closed.");
+	}
+	
+	public static CustomHealth getPlugin() {
+		return plugin;
+	}
+	
+	public static void reloadPlugin() {
+		plugin.saveConfig();
+		plugin.reloadConfig();
+		
 	}
 }
