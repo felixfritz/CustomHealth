b2eba505a484d17d5ed9c52b46744d4e64427ff6
felixfritz <geigenmusikant@yahoo.de>
2013-05-23 00:00:21 +0200

    Added EffectValue to the FoodValue list

--- src/at/felixfritz/customhealth/foodtypes/FoodValue.java
+++ src/at/felixfritz/customhealth/foodtypes/FoodValue.java
@@ -1,5 +1,8 @@
 package at.felixfritz.customhealth.foodtypes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Material;
 
 /**
@@ -13,27 +16,29 @@ public class FoodValue {
 	private Material foodName;
 	private int regenHearts;
 	private int regenHunger;
+	private List<EffectValue> effects;
 	
 	/**
 	 * Set it to a specific material. It will regenerate 0 hearts and 0 hunger bars!
 	 * @param foodName
 	 */
 	public FoodValue(Material foodName) {
-		this.foodName = foodName;
-		regenHearts = 0;
-		regenHunger = 0;
+		new FoodValue(foodName, regenHearts, regenHunger, new ArrayList<EffectValue>());
 	}
 	
 	public FoodValue(Material foodName, int regenHearts, int regenHunger) {
+		new FoodValue(foodName, regenHearts, regenHunger, new ArrayList<EffectValue>());
+	}
+	
+	public FoodValue(Material foodName, int regenHearts, int regenHunger, List<EffectValue> effects) {
 		this.foodName = foodName;
 		this.regenHearts = regenHearts;
 		this.regenHunger = regenHunger;
+		this.effects = effects;
 	}
 	
-	
-	
 	public String getName() {
-		return this.foodName.toString();
+		return this.foodName.name();
 	}
 	
 	public Material getFood() {
@@ -54,5 +59,27 @@ public class FoodValue {
 	
 	public void setRegenHunger(int regenHunger) {
 		this.regenHunger = regenHunger;
+	}
+	
+	public void setEffects(List<EffectValue> effects) {
+		this.effects = effects;
+	}
+	
+	public List<EffectValue> getEffects() {
+		return effects;
+	}
+	
+	public void addEffect(EffectValue effect) {
+		this.effects.add(effect);
+	}
+	
+	public boolean removeEffect(EffectValue effect) {
+		for(EffectValue value : effects) {
+			if(value.equals(effect)) {
+				effects.remove(effect);
+				return true;
+			}
+		}
+		return false;
 	}
 }
