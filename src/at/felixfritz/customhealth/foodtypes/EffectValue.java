9eaa206c69a3fa8602465b1ff0b4c9e683d4f334
felixfritz <geigenmusikant@yahoo.de>
2013-05-27 01:31:34 +0200

    Instead of storing the potion effect type, it stores an int
    Originally the EffectValue stored the PotionEffectType as effect. I
    changed it to an int, because I wanted to include giving XP levels that
    way. Since there are no PotionEffects that give XP values, I'm just
    cheating a little bit by saving the int. 0 means that the effect is
    giving XP values, everything else stays the same.

--- src/at/felixfritz/customhealth/foodtypes/EffectValue.java
+++ src/at/felixfritz/customhealth/foodtypes/EffectValue.java
@@ -2,26 +2,27 @@ package at.felixfritz.customhealth.foodtypes;
 
 import org.bukkit.potion.PotionEffectType;
 
+
 public class EffectValue {
 	
-	private PotionEffectType effect;
+	private int effect;
 	private double probability;
 	private int duration;
 	private int strength;
 	
-	public EffectValue(PotionEffectType effect) {
+	public EffectValue(int effect) {
 		new EffectValue(effect, 1D, 30, 0);
 	}
 	
-	public EffectValue(PotionEffectType effect, double probability) {
+	public EffectValue(int effect, double probability) {
 		new EffectValue(effect, probability, 30, 0);
 	}
 	
-	public EffectValue(PotionEffectType effect, double probability, int duration) {
+	public EffectValue(int effect, double probability, int duration) {
 		new EffectValue(effect, probability, duration, 0);
 	}
 	
-	public EffectValue(PotionEffectType effect, double probability, int duration, int strength) {
+	public EffectValue(int effect, double probability, int duration, int strength) {
 		this.effect = effect;
 		setProbability(probability);
 		setDuration(duration);
@@ -52,8 +53,15 @@ public class EffectValue {
 		return strength;
 	}
 	
-	public PotionEffectType getEffect() {
+	public int getEffect() {
 		return effect;
+	}
+	
+	public String getName() {
+		if(effect == 0)
+			return duration + " XP";
+		
+		return PotionEffectType.getById(effect).getName();
 	}
 	
 	@Override
