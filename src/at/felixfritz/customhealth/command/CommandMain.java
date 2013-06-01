d962233ee9158b1fdad70e5dc6c9efe3b83c14db
felixfritz <geigenmusikant@yahoo.de>
2013-06-01 20:34:20 +0200

    Removed unneccessary permission checking, changed Material to ItemStack

--- src/at/felixfritz/customhealth/command/CommandMain.java
+++ src/at/felixfritz/customhealth/command/CommandMain.java
@@ -10,6 +10,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.permissions.Permission;
 
 import at.felixfritz.customhealth.CustomHealth;
@@ -103,19 +104,15 @@ public class CommandMain implements CommandExecutor, TabCompleter {
 	 * The list is going to be extended in the future.
 	 */
 	private void sendHelpMenu() {
-		if(sender.hasPermission("customhealth.commands.help")) {
-			sender.sendMessage(descr + "-----------[ " + highlight + plugin.getDescription().getName() + descr + " ]-----------");
-			if(sender.hasPermission("customhealth.commands.info"))
-				sender.sendMessage(suffix + "info" + descr + ": Get health/food value/effects from item.");
-			if(sender.hasPermission("customhealth.commands.reload"))
-				sender.sendMessage(suffix + "reload" + descr + ": Reload the config file.");
-			if(sender.hasPermission("customhealth.commands.reset"))
-				sender.sendMessage(suffix + "reset" + descr + ": Reset all food items to their original value.");
-			if(sender.hasPermission("customhealth.commands.plugin"))
-				sender.sendMessage(suffix + "plugin" + descr + ": Informations about CustomHealth.");
-		} else {
-			sender.sendMessage(plugin.getConfig().getString("messages.no-permission"));
-		}
+		sender.sendMessage(descr + "-----------[ " + highlight + plugin.getDescription().getName() + descr + " ]-----------");
+		if(sender.hasPermission("customhealth.commands.info"))
+			sender.sendMessage(suffix + "info" + descr + ": Get health/food value/effects from item.");
+		if(sender.hasPermission("customhealth.commands.reload"))
+			sender.sendMessage(suffix + "reload" + descr + ": Reload the config file.");
+		if(sender.hasPermission("customhealth.commands.reset"))
+			sender.sendMessage(suffix + "reset" + descr + ": Reset all food items to their original value.");
+		if(sender.hasPermission("customhealth.commands.plugin"))
+			sender.sendMessage(suffix + "plugin" + descr + ": Informations about CustomHealth.");
 	}
 	
 	
@@ -133,9 +130,10 @@ public class CommandMain implements CommandExecutor, TabCompleter {
 			Player p = (Player) sender;
 			//Check first, if the item in hand is edible. If so,
 			//call the informPlayer method in the InfoCommand class.
-			Material m = p.getItemInHand().getType();
+			ItemStack s = p.getItemInHand();
+			Material m = s.getType();
 			if(m.isEdible() || m.equals(Material.CAKE_BLOCK))
-				CommandInfo.informPlayer(p, m);
+				CommandInfo.informPlayer(p, s);
 			else
 				Messenger.sendMessage(plugin.getConfig().getString("messages.item-not-edible").replaceAll("<food>", m.toString()), p);
 			return true;
@@ -145,10 +143,15 @@ public class CommandMain implements CommandExecutor, TabCompleter {
 		//If the player has added an argument, then he'll get the information about the food in the 2. argument
 		if(args.length == 2) {
 			try {
-				String item = (args[1].equalsIgnoreCase("cake_slice")) ? "CAKE_BLOCK" : args[1].toUpperCase();
-				Material type = Material.getMaterial(item.toUpperCase());
+				ItemStack stack = new ItemStack((args[1].equalsIgnoreCase("enchanted_golden_apple")) ? Material.valueOf("GOLDEN_APPLE") : 
+					Material.valueOf(args[1].toUpperCase()));
+				
+				if(args[1].equalsIgnoreCase("enchanted_golden_apple"))
+					stack.getData().setData((byte) 1);
+				
+				Material type = stack.getType();
 				if(type.isEdible() || type.equals(Material.CAKE_BLOCK))
-					CommandInfo.informPlayer(sender, Material.getMaterial(args[1].toUpperCase()));
+					CommandInfo.informPlayer(sender, stack);
 				else
 					Messenger.sendMessage(plugin.getConfig().getString("messages.item-not-edible").replaceAll("<food>", args[1]), sender);
 			} catch(NullPointerException e) {
@@ -166,14 +169,10 @@ public class CommandMain implements CommandExecutor, TabCompleter {
 	 * The help menu for /chealth get
 	 */
 	private void sendGetHelpMenu() {
-		if(sender.hasPermission("customhealth.commands.get")) {
-			sender.sendMessage(descr + "-----------[ " + highlight + plugin.getDescription().getName() + " /get" + descr + " ]-----------");
-			sender.sendMessage(suffix + "get" + descr + ": Get the values of the food item in hand.");
-			sender.sendMessage(suffix + "get [food]" + descr + ": Get the values about [food].");
-		} else {
-			sender.sendMessage(plugin.getConfig().getString("messages.no-permission"));
-		}
-		
+		sender.sendMessage(descr + "-----------[ " + highlight + plugin.getDescription().getName() + " /get" + descr + " ]-----------");
+		sender.sendMessage(suffix + "info" + descr + ": Get the values of the food item in hand.");
+		sender.sendMessage(suffix + "info [food]" + descr + ": Get the values about [food].");
+	
 	}
 	
 	
