c82c7524cd877068ca1dbfca1c62a343751555da
felixfritz <geigenmusikant@yahoo.de>
2013-06-11 13:32:52 +0200

    Updated for Multiworlds support

--- src/at/felixfritz/customhealth/event/HealthEvent.java
+++ src/at/felixfritz/customhealth/event/HealthEvent.java
@@ -7,6 +7,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent.RegainReason;
 
+import at.felixfritz.customhealth.CustomHealth;
+
 /**
  * This listener is being called when regain-health is set to false<br>
  * -> Heart level won't change if you're saturated, only works with enchantments, potions, etc.
@@ -17,6 +19,9 @@ public class HealthEvent implements Listener {
 	
 	@EventHandler (priority = EventPriority.HIGH)
 	public void onRegainHealth(EntityRegainHealthEvent evt) {
+		
+		if(CustomHealth.isHeartLevelChanging(evt.getEntity().getWorld()))
+			return;
 		
 		//just check, whether or not the Entity is a player and the player is satiated, which is why he didn't regain hearts
 		if(evt.getRegainReason().equals(RegainReason.SATIATED) && (evt.getEntity() instanceof Player)) {
