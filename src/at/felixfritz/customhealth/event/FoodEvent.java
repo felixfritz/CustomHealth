733dd14c25119403c0c26cb0f948eb13af36641e
felixfritz <geigenmusikant@yahoo.de>
2013-06-11 13:32:28 +0200

    Updated for Multiworlds support

--- src/at/felixfritz/customhealth/event/FoodEvent.java
+++ src/at/felixfritz/customhealth/event/FoodEvent.java
@@ -5,16 +5,32 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import at.felixfritz.customhealth.CustomHealth;
 
 public class FoodEvent implements Listener {
 	
 	@EventHandler (priority = EventPriority.HIGH)
 	public void onFoodLevelChange(FoodLevelChangeEvent evt) {
-		evt.setCancelled(true);
+		
 		Player p = (Player) evt.getEntity();
 		
+		if(CustomHealth.isFoodLevelChanging(p.getWorld()))
+			return;
+		
+		evt.setCancelled(true);
+		
 		//If set to 20, the player isn't able to eat anything anymore.
-		p.setFoodLevel(19);
+		p.setFoodLevel(CustomHealth.getMaxFoodLevel(p.getWorld()));
 		p.setSaturation(10);
+	}
+	
+	@EventHandler
+	public void onPlayerDeath(PlayerRespawnEvent evt) {
+		
+		if(!CustomHealth.isFoodLevelChanging(evt.getPlayer().getWorld()))
+			evt.getPlayer().setFoodLevel(CustomHealth.getMaxFoodLevel(evt.getPlayer().getWorld()));
+		
 	}
 }
